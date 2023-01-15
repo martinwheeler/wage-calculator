8afb6e73282f9f1436b637170c593457e50bd52e
Martin Wheeler <martin@martinwheeler.com.au>
2023-01-15 10:28:30 +1000

    fix: remove document selector & conditionally show error

--- src/Components/FinanceDataInput/FinanaceDataInput.tsx
+++ src/Components/FinanceDataInput/FinanaceDataInput.tsx
@@ -1,42 +1,43 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import './FinanceDataInput.scss';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import "./FinanceDataInput.scss";
 
-const FinanceDataInput = (prop: any) => {
-    const [value, setValue] = useState('');
-    const [error, setErrorState] = useState (false);
-    const labelErrorMessage = document.getElementById('label');
-
-    
-
-    const handleChange= (e: any) => {
-        setValue(e.target.value);
-        if (/[^a-zA-Z\s]/.test(e.target.value)){
-            setErrorState(false);
-        }
-        else {
-            setErrorState(true);
-        }
-    }
-
-    useEffect(() => {
-        if (labelErrorMessage !== null && error === true){
-            labelErrorMessage.innerHTML='This field is required.'
-        }
-    })
-
-
-    return(
-        <div className='centered'>
-            <div className='group'>
-                <input value={value} type={prop.type} onChange={handleChange} id='name' required={prop.required}/>
-                <label htmlFor="name" className={error ? 'redLabel' : 'label'} id='label'>{prop.label}</label>
-                <div className={error ? 'errorBar' : 'bar'}></div>
-            </div>
-        </div>
-
-    )
+interface Props {
+  type: string;
+  required: boolean;
+  label: string;
+  errorMessage?: string;
 }
 
-export default FinanceDataInput;
+const FinanceDataInput = (prop: Props) => {
+  const [value, setValue] = useState("");
 
+  const handleChange = (e: any) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div className="centered">
+      <div className="group">
+        <input
+          value={value}
+          type={prop.type}
+          onChange={handleChange}
+          id="name"
+          required={prop.required}
+        />
+        <label
+          htmlFor="name"
+          className={prop.errorMessage ? "redLabel" : "label"}
+          id="label"
+        >
+          {prop.label}
+        </label>
+        {prop.errorMessage ? prop.errorMessage : ""}
+        <div className={prop.errorMessage ? "errorBar" : "bar"}></div>
+      </div>
+    </div>
+  );
+};
+
+export default FinanceDataInput;
