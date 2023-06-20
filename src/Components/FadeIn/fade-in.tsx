b48bbcab8bf5cb5706e172a6ae36a0916f7c754b
Martin Wheeler <martin@martinwheeler.com.au>
2023-06-20 19:51:00 +1000

    fix: only return classname when defined

--- src/Components/FadeIn/fade-in.tsx
+++ src/Components/FadeIn/fade-in.tsx
@@ -1,25 +1,26 @@
 import { ReactNode } from "react";
 
 export const FadeIn = ({
-    children,
-    className,
-    delay = "6.5s",
-    duration = "0.5s",
-    ...props
+  children,
+  className,
+  delay = "6.5s",
+  duration = "0.5s",
+  ...props
 }: {
-    children: ReactNode;
-    className?: string;
-    delay?: string;
-    duration?: string;
-    [prop: string]: any;
+  children: ReactNode;
+  className?: string;
+  delay?: string;
+  duration?: string;
+  [prop: string]: any;
 }) => {
-    return (
-        <div
-            {...props}
-            className={`${className} animate-[fadeIn_${duration}_ease-in-out_${delay}_forwards] opacity-0`}
-        >
-            {children}
-        </div>
-    );
+  return (
+    <div
+      {...props}
+      className={`${
+        className ? className : ""
+      } animate-[fadeIn_${duration}_ease-in-out_${delay}_forwards] opacity-0`}
+    >
+      {children}
+    </div>
+  );
 };
-
